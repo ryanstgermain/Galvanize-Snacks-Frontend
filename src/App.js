966035beb3e7ee45c5161d09b9fcdf72e27eb6fd
keschaefer <keschaefer@gmail.com>
2018-12-07 12:12:08 -0700

    added modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './Components/Card.js'
+import Modal from './Components/Modal.js'
 
 class App extends Component {
   constructor () {
@@ -11,13 +12,23 @@ class App extends Component {
           "description":"Mauris lacinia sapien quis libero.","is_perishable": true,
           "price":"8.44","img":"https://images.unsplash.com/photo-1459663148042-6d8cb9340411?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&s=44153beb9e2d61c2970e93f8d72a862e",
           "reviews": "Yay for Pork!"
-        }]
+        }],
+        showModal: true
       }
   }
+  // showModal = () => {
+  //   this.setState({ showModal: true})
+  // }
+
+  hideModal = () => {
+    this.setState({ showModal: false})
+  }
+
   render() {
     return (
       <div className="App">
       <Card indSnack= {this.state.indSnack}/>
+      <Modal hideModal= {this.hideModal} showModal= {this.state.showModal}/>
       </div>
     );
   }
