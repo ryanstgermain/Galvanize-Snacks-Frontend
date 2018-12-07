e550a3e47aa41e1279ce65149b81dd22406c6194
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-12-07 11:39:35 -0700

    added list-items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './Components/Header'
+import SnackList from './Components/SnackList'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-      
+        <Header />
+        <div className="row">
+          <SnackList />
+        </div>
       </div>
     );
   }
---
+++ src/Components/SnackList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SnackList = (props) => {
+    return (
+        <div className="col-4">
+            <div className="list-group">
+                <a href="#" className="list-group-item list-group-item-action rounded">Cras justo odio</a>
+                <a href="#" className="list-group-item list-group-item-action rounded">Dapibus ac facilisis in</a>
+                <a href="#" className="list-group-item list-group-item-action rounded">Morbi leo risus</a>
+                <a href="#" className="list-group-item list-group-item-action rounded">Porta ac consectetur ac</a>
+                <a href="#" className="list-group-item list-group-item-action rounded">Vestibulum at eros</a>
+            </div>
+        </div>
+    )
+}
+
+
+export default SnackList
