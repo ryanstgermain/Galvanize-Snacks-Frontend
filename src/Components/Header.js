d535f3b722b79b4cd66278e5a79521719415c124
Brandon Redshirt <brandon.redshirt@gmail.com>
2018-12-07 11:09:00 -0700

    created Header and added BS CDN

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = (props) => {
+    return (
+        <nav className="navbar navbar-dark bg-primary">
+            <span className="navbar-brand mb-0 h1">gSnacks</span>
+        </nav>
+    )
+}
+
+export default Header
